1ce73ca88b62a19af58450ae1723e4e272891167
Alex Sofronie <alsofronie@gmail.com>
2016-02-13 15:04:18 +0200

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            compile: {
+                options: {
+                    paths: ['src']
+                },
+                files: {
+                    "dist/scroll-icon.css": "src/scroll-icon.less"
+                }
+            }
+        },
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1,
+                sourceMap: true
+            },
+            target: {
+                files: {
+                    'dist/scroll-icon.min.css': ['dist/scroll-icon.css']
+                }
+            }
+        },
+        watch: {
+            styles: {
+                files: ['src/**/*.less'],
+                tasks: ['less', 'cssmin'],
+                options: {
+                    spawn: false,
+                },
+            },
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-less');
+};
